335ded1f9eb0962084554aab940b27967807eace
Owen Jones <owen@owenjones.net>
2020-03-19 03:34:28 +0000

    channel logging widget

--- src/widgets/logger/handlers/message.js
+++ src/widgets/logger/handlers/message.js
@@ -1,6 +1,27 @@
-// sheets = []
-// doc.sheetsByIndex.map(s => sheets.append(s.name))
-//
-module.exports = async message => {
+var { getDoc, hasSheet, getSheet, addSheet } = require('../caches/sheetCache')
 
+module.exports = async message => {
+  var doc = getDoc()
+  var sheet
+
+  if(!hasSheet(message.channel.name)) {
+    sheet = await doc.addSheet({
+      title: message.channel.name,
+      headerValues: ['time', 'userId', 'userName', 'message', 'embeds', 'bot']
+     })
+    addSheet(message.channel.name, sheet.sheetId)
+    sheet = await doc.sheetsById[sheet.sheetId]
+  }
+  else {
+    sheet = await doc.sheetsById[getSheet(message.channel.name)]
+  }
+
+  await sheet.addRow({
+    time     : message.createdTimestamp.toString(),
+    userId   : message.author.id,
+    userName : message.author.username,
+    message  : message.content,
+    embeds   : JSON.stringify(message.embeds),
+    bot      : message.author.bot
+  })
 }
--- src/widgets/logger/handlers/ready.js
+++ src/widgets/logger/handlers/ready.js
@@ -1,7 +1,13 @@
-const { db, table } = require('../config')
+var { getDoc, getSheets, setSheets } = require('../caches/sheetCache')
 
 module.exports = async client => {
-  
+  var doc = getDoc()
+  var sheets = getSheets()
+
+  await doc.useServiceAccountAuth(require('../../../../google-service-account'))
+  await doc.loadInfo()
+  doc.sheetsByIndex.map(sheet => sheets.set(sheet.title, sheet.sheetId))
+  setSheets(sheets)
 
   console.log('logger: ready')
 }
