122e07bc9d6c5ff2acf3059f50865f316dcd1a25
Owen Jones <owen@owenjones.net>
2020-03-19 14:35:42 +0000

    now properly logs DMs and attachments

--- src/widgets/logger/handlers/message.js
+++ src/widgets/logger/handlers/message.js
@@ -1,27 +1,31 @@
 var { getDoc, hasSheet, getSheet, addSheet } = require('../caches/sheetCache')
 
 module.exports = async message => {
+  if(message.channel.type == 'text') sheetTitle = message.channel.name
+  else if(message.channel.type == 'dm') sheetTitle = 'dm-' + message.author.username
+
   var doc = getDoc()
   var sheet
 
-  if(!hasSheet(message.channel.name)) {
+  if(!hasSheet(sheetTitle)) {
     sheet = await doc.addSheet({
-      title: message.channel.name,
-      headerValues: ['time', 'userId', 'userName', 'message', 'embeds', 'bot']
+      title: sheetTitle,
+      headerValues: ['time', 'userId', 'userName', 'message', 'embeds', 'attachments', 'bot']
      })
-    addSheet(message.channel.name, sheet.sheetId)
+    addSheet(sheetTitle, sheet.sheetId)
     sheet = await doc.sheetsById[sheet.sheetId]
   }
   else {
-    sheet = await doc.sheetsById[getSheet(message.channel.name)]
+    sheet = await doc.sheetsById[getSheet(sheetTitle)]
   }
 
   await sheet.addRow({
-    time     : message.createdTimestamp.toString(),
-    userId   : message.author.id,
-    userName : message.author.username,
-    message  : message.content,
-    embeds   : JSON.stringify(message.embeds),
-    bot      : message.author.bot
+    time        : message.createdTimestamp.toString(),
+    userId      : message.author.id,
+    userName    : message.author.username,
+    message     : message.content,
+    embeds      : JSON.stringify(message.embeds),
+    attachments : JSON.stringify(message.attachments),
+    bot         : message.author.bot
   })
 }
