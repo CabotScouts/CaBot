ea1ea8ba2ad6b78f606dcde3c3249f1796bc8cc6
Owen Jones <owen@owenjones.net>
2020-03-19 14:36:41 +0000

    now only checks in text channels, and doesn't reply to the user

--- src/widgets/sanitise/handlers/message.js
+++ src/widgets/sanitise/handlers/message.js
@@ -1,6 +1,7 @@
 const words = require('../words.json')
 
 module.exports = async message => {
+  if(message.channel.type != 'text') return
   if(message.author.bot) return
   if(message.system) return
   if(message.member.roles.cache.find(r => r.name === 'leader')) return
@@ -12,7 +13,7 @@ module.exports = async message => {
     // strike 1 - send a warning message
     // strike 2 - kick user
     // strike 3 - ban user
-    message.channel.send(`:disappointed_relieved: *that's a bad word*`)
+    // message.channel.send(`:disappointed_relieved: *that's a bad word*`)
 
     message.client.channels.cache.find(ch => ch.name === 'system').send(`:warning: ${message.author} swore in ${message.channel} (${message.toString()})`)
   }
