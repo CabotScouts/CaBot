1a6d699d0c779170540a8e946a2c73ce1e099284
Owen Jones <owen@owenjones.net>
2020-03-19 01:06:00 +0000

    renamed verify helper events

--- src/widgets/automessage/handlers/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = async client => {
-  // client.channels.cache.find(ch => ch.name === 'system').send(`⚠️ CaBot restarted`);
-  console.log('automessage: ready');
-};
--- src/widgets/verifyhelper/handlers/guildMemberAdd.js
+++ src/widgets/verifyhelper/handlers/guildMemberAdd.js
@@ -4,6 +4,5 @@ module.exports = async member => {
   if(explorer || leader) return
 
   member.guild.channels.cache.find(ch => ch.name === 'verify').send(`:fleur_de_lis: Welcome to Cabot Explorers on Discord ${member} - before you can access the server we need to check that you're an Explorer - please reply here with your **name** and **Explorer Unit**, and wait for a leader to verify you`)
-
   member.guild.channels.cache.find(ch => ch.name === 'system').send(`:bell: ${member} requires verification`)
 };
