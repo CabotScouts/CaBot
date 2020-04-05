26baf2cf21e92bd615387d615ffaa12454ef2e14
Owen Jones <owen@owenjones.net>
2020-04-05 22:37:57 +0100

    fixed bug where not all joining saw the message

--- src/widgets/verifyhelper/handlers/guildMemberAdd.js
+++ src/widgets/verifyhelper/handlers/guildMemberAdd.js
@@ -3,6 +3,9 @@ module.exports = async member => {
   leader = member.roles.cache.has('leader')
   if(explorer || leader) return
 
-  member.guild.channels.cache.find(ch => ch.name === 'verify').send(`:fleur_de_lis: Welcome to Cabot Explorers on Discord ${member} - before you can access the server we need to check that you're an Explorer - please reply here with your **name** and **Explorer Unit**, and wait for a leader to verify you`)
-  member.guild.channels.cache.find(ch => ch.name === 'system').send(`:bell: ${member} requires verification`)
+  await sleep(5000)
+
+  await member.guild.channels.cache.find(ch => ch.name === 'verify').send(`:fleur_de_lis: Welcome to Cabot Explorers on Discord ${member} - before you can access the server we need to check that you're an Explorer - please reply here with your **name** and **Explorer Unit**, and wait for a leader to verify you`)
+
+  await member.guild.channels.cache.find(ch => ch.name === 'system').send(`:bell: ${member} requires verification`)
 };
