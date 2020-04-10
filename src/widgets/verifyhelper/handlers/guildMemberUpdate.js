d40fe4a05aca91c982329e43e7f80cca5a43d46f
Owen Jones <owen@owenjones.net>
2020-04-10 20:51:47 +0100

    added network

--- src/widgets/verifyhelper/handlers/guildMemberUpdate.js
+++ src/widgets/verifyhelper/handlers/guildMemberUpdate.js
@@ -1,6 +1,7 @@
 module.exports = async (oldMember, newMember) => {
   explorerAdded = !oldMember.roles.cache.has('689256639265636382') && newMember.roles.cache.has('689256639265636382')
   leaderAdded = !oldMember.roles.cache.has('689256880836444182') && newMember.roles.cache.has('689256880836444182')
+  networkAdded = !oldMember.roles.cache.has('694565693181526036') && newMember.roles.cache.has('694565693181526036')
 
   if(explorerAdded) {
     newMember.guild.channels.cache.find(ch => ch.name === 'chat').send(`:fleur_de_lis: Welcome to Cabot Explorers on Discord ${newMember} - say hello!`)
@@ -9,5 +10,9 @@ module.exports = async (oldMember, newMember) => {
 
   if(leaderAdded) {
     newMember.guild.channels.cache.find(ch => ch.name === 'leaders').send(`:fleur_de_lis: Welcome to the leaders lounge ${newMember}`)
+  }
+
+  if(networkAdded) {
+    newMember.guild.channels.cache.find(ch => ch.name === 'network').send(`:fleur_de_lis: Welcome to the network chat ${newMember}`)
   }
 };
