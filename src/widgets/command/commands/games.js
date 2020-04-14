db5f15181a6c0fc968b1045da8e578ea9e19cf07
Owen Jones <owen@owenjones.net>
2020-04-14 20:01:21 +0100

    fixed finding role from ID

--- src/widgets/command/commands/games.js
+++ src/widgets/command/commands/games.js
@@ -8,10 +8,10 @@ module.exports = {
   cooldown: 10,
   disabled: false,
   messageExecute: async (message, args) => {
-    gameID = 699660460546588812
-    role = message.guild.roles.cache.find(r => r.id === gameID)
+    gameID = "699660460546588812"
+    role = message.guild.roles.cache.find(r => r.id == gameID)
 
-    if(message.member.roles.has(gameID)) {
+    if(message.member.roles.cache.has(gameID)) {
       message.member.removeRole(role).then(
         message.channel.send({
           "embed" : {
