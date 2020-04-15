ff553c49013bf0a0c18198c42ed8cab1d34cb6c7
Owen Jones <owen@owenjones.net>
2020-04-15 23:12:21 +0100

    show full leaderboard

--- src/widgets/command/commands/leaderboard.js
+++ src/widgets/command/commands/leaderboard.js
@@ -12,30 +12,29 @@ module.exports = {
   cooldown: 120,
   disabled: false,
   messageExecute: async (message, args) => {
-    var title = ":trophy: Leaderboard :trophy:"
     await storage.init()
     var leaderboard = await storage.getItem('leaderboard')
 
     if(leaderboard.length > 0) {
       leaderboard.sort((a, b) => (b.points - a.points))
-      var num = Math.min(leaderboard.length, 3)
+      // var num = Math.min(leaderboard.length, 3)
       const medals = [':first_place:', ':second_place:', ':third_place:']
       var fields = []
-      for(i = 0; i < num; i++) {
+      for(i = 0; i < leaderboard.length; i++) {
         var user = message.guild.members.cache.get(leaderboard[i].id)
         var points = leaderboard[i].points
         var name = `<@!${leaderboard[i].id}>`
         var plural = (points > 1) ? 'points' : 'point'
+        var medal = (i <= 3) ? medals[i] : ':tomato:'
 
         fields.push({
-          "name" : `${medals[i]} ${points} ${plural}`,
+          "name" : `${medal} ${points} ${plural}`,
           "value" : `${name}\n`
         })
       }
 
       message.channel.send({
         "embed": {
-          // "title" : title,
           "color": 3447003,
           "fields": fields
         }
@@ -44,7 +43,6 @@ module.exports = {
     else {
       message.channel.send({
         "embed": {
-          // "title" : title,
           "description": "The leaderboard is empty!",
           "color": 3447003
         }
