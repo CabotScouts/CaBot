24e48cca037deeee77baa7903103b8e11d8eac18
Owen Jones <owen@owenjones.net>
2020-04-16 00:10:30 +0100

    topic command improvements

--- src/widgets/command/commands/topic.js
+++ src/widgets/command/commands/topic.js
@@ -2,13 +2,14 @@ module.exports = {
   name: 'topic',
   aliases: ['t'],
   ownersOnly: false,
-  guildOnly: false,
+  guildOnly: true,
   requireArgs: true,
   deleteCommand: false,
   cooldown: 10,
   disabled: false,
   messageExecute: async (message, args) => {
-    message.guild.channels.create(args[0], { type: 'text', parent: '689309599928418392'}).then(channel => message.channel.send({
+    var topic = args.join('-').toLowerCase().replace(/[$&+,:;=?@#|'<>.^*()%!_]/g, '-')
+    message.guild.channels.create(topic, { type: 'text', parent: '689309599928418392'}).then(channel => message.channel.send({
       "embed" : {
         "description" : `:ledger: new topic (${channel}) created`,
         "color" : 7419530
