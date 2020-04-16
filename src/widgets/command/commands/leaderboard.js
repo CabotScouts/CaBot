fc334d4ce06b6a049bf51a26ea34f4fee6dc93f7
Owen Jones <owen@owenjones.net>
2020-04-16 13:37:26 +0100

    tidied up leaderboard code some more

--- src/widgets/command/commands/leaderboard.js
+++ src/widgets/command/commands/leaderboard.js
@@ -6,10 +6,10 @@ module.exports = {
   name: 'leaderboard',
   aliases: ['board'],
   ownersOnly: false,
-  guildOnly: false,
+  guildOnly: true,
   requireArgs: false,
   deleteCommand: false,
-  cooldown: 120,
+  cooldown: 0,
   disabled: false,
   messageExecute: async (message, args) => {
     await storage.init()
@@ -17,20 +17,19 @@ module.exports = {
 
     if(leaderboard.length > 0) {
       leaderboard.sort((a, b) => (b.points - a.points))
-      // var num = Math.min(leaderboard.length, 3)
       var num = (args[0] == "all") ? leaderboard.length : Math.min(leaderboard.length, 3)
-      const medals = [':first_place:', ':second_place:', ':third_place:']
+      const medals = [':first_place: ', ':second_place: ', ':third_place: ']
       var fields = []
+
       for(i = 0; i < num; i++) {
-        var user = message.guild.members.cache.get(leaderboard[i].id)
-        var points = leaderboard[i].points
-        var name = `<@!${leaderboard[i].id}>`
-        var plural = (points > 1) ? 'points' : 'point'
         var medal = (i < 3) ? medals[i] : ''
+        var points = leaderboard[i].points
+        var plural = (points > 1) ? 'points' : 'point'
+        var name = `<@!${leaderboard[i].id}>`
 
         fields.push({
-          "name" : `${medal} ${points} ${plural}`,
-          "value" : `${name}\n`
+          "name" : `${medal}${points} ${plural}`,
+          "value" : `${name}\n\n`
         })
       }
 
