5185aadf3e215f29920e1e03dacc77b488e1548c
Owen Jones <owen@owenjones.net>
2020-05-28 23:31:26 +0100

    Changed way role toggling works so we can have additional roles

---
+++ src/widgets/command/util/toggleRole.js
@@ -0,0 +1,24 @@
+module.exports = async (message, roleID = []) => {
+  role = message.guild.roles.cache.find(role => role.id == roleID)
+
+  if(message.member.roles.cache.has(role.id)) {
+    message.member.roles.remove(role.id).then(
+      message.channel.send({
+        "embed" : {
+          "description" : `Removed from ${role.mention}` ,
+          "color" : 15105570
+        }
+      })
+    )
+  }
+  else {
+    message.member.roles.add(roleID).then(
+      message.channel.send({
+        "embed" : {
+          "description" : `Added to ${role.mention}`,
+          "color" : 15105570
+        }
+      })
+    )
+  }
+}
