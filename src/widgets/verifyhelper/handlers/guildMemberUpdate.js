fda23c8c5c0f9bfc312146a7b2207e7a63134fbf
Owen Jones <owen@owenjones.net>
2020-07-29 14:49:12 +0100

    update verification messages for log

--- src/widgets/verifyhelper/handlers/guildMemberUpdate.js
+++ src/widgets/verifyhelper/handlers/guildMemberUpdate.js
@@ -6,18 +6,18 @@ module.exports = async (oldMember, newMember) => {
   if(explorerAdded) {
     newMember.guild.channels.cache.find(ch => ch.id == '689271687078084707').send(`<:scout:693531748696326245> Welcome to Cabot Explorers on Discord ${newMember} - say hello!`)
 
-    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified`)
+    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified as an Explorer`)
   }
 
   if(leaderAdded) {
     newMember.guild.channels.cache.find(ch => ch.id == '689552904511553591').send(`<:scout:693531748696326245> Welcome to the leaders lounge ${newMember}`)
 
-    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified`)
+    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified as a leader`)
   }
 
   if(networkAdded) {
     newMember.guild.channels.cache.find(ch => ch.name === 'gromit').send(`<:scout:693531748696326245> Welcome to the network chat ${newMember}`)
 
-    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified`)
+    newMember.guild.channels.cache.find(ch => ch.id == '689298055517962330').send(`:star: ${newMember} was verified as a Networker`)
   }
 };
