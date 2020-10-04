b04ae7a6947e9d9189d0883e7c02fc5ac3746f9a
Owen Jones <owen@owenjones.net>
2020-10-05 00:40:49 +0100

    finished commands for making new topics and messaging other channels

--- cogs/base.py
+++ cogs/base.py
@@ -1,3 +1,5 @@
+import re
+
 import discord
 from discord.ext import commands
 
@@ -71,19 +73,37 @@ class BaseCommands(commands.Cog) :
     @commands.command()
     @commands.guild_only()
     @commands.check(isLeader)
-    async def announce(self, ctx) :
-        print("makes announcement")
+    async def announce(self, ctx, *message) :
+        """send an announcement message in the main chat channel"""
+        channel = ctx.guild.get_channel(689271687078084707)
+
+        embed = discord.Embed(
+            title = ":loudspeaker:",
+            description = ' '.join(message),
+            color = 16580705,
+        )
+
+        await channel.send(embed=embed)
 
     @commands.command()
     @commands.guild_only()
     @commands.check(isLeader)
-    async def message(self, ctx) :
-        pass
+    async def message(self, ctx, *args) :
+        channels = ctx.message.channel_mentions
+
+        if len(channels) > 0 :
+            messageLength = (len(args) - len(channels))
+            message = ' '.join(args[len(channels):])
+
+            for channel in channels :
+                await channel.send(message)
 
     @commands.command()
     @commands.guild_only()
     async def topic(self, ctx, *topic) :
-        pass
+        category = ctx.guild.get_channel(689309599928418392)
+        topic = '-'.join([ re.sub(r'[^\w]', '', word) for word in topic ]).lower()
+        await ctx.guild.create_text_channel(topic, category = category)
 
     # @commands.command()
     # @commands.guild_only()
