a28aae97d83a346bbaaa5de537e69cb686261fab
Owen Jones <owen@owenjones.net>
2020-10-04 01:46:12 +0100

    enabled extensions

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from peewee import SqliteDatabase
 
 load_dotenv()
+
+__version__ = "2.0.a"
 
 class CaBot(commands.Bot) :
 
@@ -12,9 +15,10 @@ class CaBot(commands.Bot) :
 
     def __init__(self, **options) :
         self.prefix = options["command_prefix"]
+        self.version = __version__
 
         if "DB" in options :
-            self.db = options["DB"]
+            self.db = SqliteDatabase(options["DB"])
 
         super().__init__(**options)
 
@@ -22,11 +26,11 @@ class CaBot(commands.Bot) :
 if __name__ == "__main__" :
     bot = CaBot(db=os.getenv("DB"), command_prefix=os.getenv("PREFIX"))
     bot.load_extension("cogs.base")
-    # bot.load_extension("cogs.dev")
-    # bot.load_extension("cogs.logger")
+    bot.load_extension("cogs.dev")
+    bot.load_extension("cogs.logger")
     bot.load_extension("cogs.roles")
-    # bot.load_extension("cogs.verify_helper")
-    # bot.load_extension("cogs.sanitise")
-    # bot.load_extension("cogs.points")
-    # bot.load_extension("cogs.social_media")
+    bot.load_extension("cogs.verify_helper")
+    bot.load_extension("cogs.sanitise")
+    bot.load_extension("cogs.points")
+    bot.load_extension("cogs.social_media")
     bot.run(os.getenv("TOKEN"))
