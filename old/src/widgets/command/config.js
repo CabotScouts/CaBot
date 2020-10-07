dc44d4632482643c8f86af14db59723cecace99a
Owen Jones <owen@owenjones.net>
2020-10-08 00:05:04 +0100

    removed old version of the bot

--- old/src/widgets/command/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  prefixes: ['!'],
-  commandDelimiter: ',,',
-  commandLimit: 3,
-  owners: ['350358952334196738', '644221719384555550']
-};
--- old/src/widgets/verifyhelper/handlers/message.js
+++
@@ -1,5 +0,0 @@
-module.exports = async message => {
-  if(message.channel.id == '689264900044095558' && message.content == '<#689264900044095558>') {
-    message.channel.send(`Nice one ${message.author}, that's how you send a message - please now send a message with your **name** and **Explorer Unit** so we can verify you`)
-  }
-}
