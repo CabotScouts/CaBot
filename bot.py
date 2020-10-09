671a59cff575a0ae397d694123484a9583d5dcb2
Owen Jones <owen@owenjones.net>
2020-10-09 23:50:27 +0100

    added defaults for .env values

--- bot.py
+++ bot.py
@@ -11,21 +11,23 @@ __version__ = "2.0"
 
 class CaBot(commands.Bot) :
 
-    db = None
-
     def __init__(self, **options) :
         self.prefix = options["command_prefix"]
         self.version = __version__
 
-        if "db" in options :
-            self.db = SqliteDatabase(options["db"])
-            self.db.connect()
+        self.db = SqliteDatabase(options["db"])
+        self.db.connect()
 
         super().__init__(**options)
 
 
 if __name__ == "__main__" :
-    bot = CaBot(db=os.getenv("DB"), command_prefix=os.getenv("PREFIX"), intents=discord.Intents.all())
+    bot = CaBot(
+        db=os.getenv("DB", ":memory:"),
+        command_prefix=os.getenv("PREFIX", "?"),
+        intents=discord.Intents.all()
+    )
+
     bot.load_extension("cogs.base")
     bot.load_extension("cogs.dev")
     bot.load_extension("cogs.logger")
@@ -35,4 +37,5 @@ if __name__ == "__main__" :
     bot.load_extension("cogs.points")
     bot.load_extension("cogs.social_media")
     bot.load_extension("cogs.count")
+
     bot.run(os.getenv("TOKEN"))
